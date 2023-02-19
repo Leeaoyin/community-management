5a3c907fb1fbb5d3b94286fe031db30ec863511e
leeaoyin <lee_ay@163.com>
2023-02-19 20:49:35 +0800

    feat: complete order service

--- src/main/java/org/management/core/domain/service/impl/VaccineOrderServiceImpl.java
+++ src/main/java/org/management/core/domain/service/impl/VaccineOrderServiceImpl.java
@@ -4,9 +4,14 @@ import org.management.core.application.common.param.dto.VaccineDTO;
 import org.management.core.domain.service.VaccineOrderService;
 import org.management.core.infrastructure.repository.mapper.VaccineOrderMapper;
 import org.management.core.infrastructure.repository.po.User;
+import org.management.core.infrastructure.repository.po.VaccineOrder;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
+@Service
 public class VaccineOrderServiceImpl implements VaccineOrderService {
     
     @Resource
@@ -15,6 +20,13 @@ public class VaccineOrderServiceImpl implements VaccineOrderService {
 
     @Override
     public Boolean order(User user, VaccineDTO vaccineDTO) {
-        return null;
+        VaccineOrder vaccineOrder = new VaccineOrder();
+        vaccineOrder.setUserId(user.getId());
+        vaccineOrder.setPhone(vaccineDTO.getPhone());
+        vaccineOrder.setVaccineName(vaccineDTO.getVaccinename());
+        vaccineOrder.setOrderTime(vaccineDTO.getOrdertime());
+        vaccineOrder.setCreateTime(new Date());
+        vaccineOrder.setState(vaccineDTO.getOrdertime().before(new Date()) ? 0 : 1);
+        return vaccineOrderMapper.insertSelective(vaccineOrder) > 0;
     }
 }
